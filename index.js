eb1a88357d797472f68a72878eaa199c9cb1e736
pushpendraraj7879 <pushpendraraj7879@gmail.com>
2024-11-29 01:02:45 +0530

    Sfsfsfsafasf

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const Admin=require("./models/admin")
 const Message=require("./models/message")
 const cors=require("cors")
 require("dotenv").config()
-app.use(cors());
+app.use(cors({
+   origin: 'http://localhost:3000'
+   
+}));
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
